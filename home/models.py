4a2e8817d8620d115e693851cfb95f9e75200940
Atsushi <todaatsushi0@gmail.com>
2019-05-08 18:36:14 +0100

    added models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    # Descriptive Info
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    link = models.CharField(null=True, max_length=100)
+    repository = models.CharField(max_length=100)
+
+    # Technical Info
+    languages = models.TextField()
+    hosting = models.CharField(max_length=100, null=True)
