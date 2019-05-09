3574e0ce8e9b91168bccdc6c94f07df2ba960430
Atsushi <todaatsushi0@gmail.com>
2019-05-09 22:29:48 +0100

    Configuration for basic model CBV

--- work/urls.py
+++ work/urls.py
@@ -5,7 +5,7 @@ from work import views as v
 urlpatterns = [
     path('', v.ProjectIndexView.as_view(), name='all-projects'),
     path('create/', v.ProjectCreateView.as_view(), name='new-project'),
-    path('<str:name>/', v.ProjectDetailView.as_view(), name='view-project'),
-    path('<str:name>/update/', v.ProjectUpdateView.as_view(), name='update-project'),
-    path('<str:name>/delete/', v.ProjectDeleteView.as_view(), name='delete-project'),
+    path('<slug:slug>/', v.ProjectDetailView.as_view(), name='view-project'),
+    path('<slug:slug>/update/', v.ProjectUpdateView.as_view(), name='update-project'),
+    path('<slug:slug>/delete/', v.ProjectDeleteView.as_view(), name='delete-project'),
 ]
--- work/views.py
+++ work/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import (DetailView, ListView, UpdateView, DeleteView, CreateView)
+from django.urls import reverse
 
 from work.forms import CreateUpdateProject
 from work.models import Project
@@ -15,22 +17,42 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = 'work/detail.html'
     context_object_name = 'project'
+    slug_field = 'url_slug'
 
 
 class ProjectUpdateView(UpdateView):
     model = Project
     template_name = 'work/update.html'
-    form_class = CreateUpdateProject
-    success_url = '/work/'
+    fields = [
+        'name', 'description', 'link', 'repository', 'languages', 'stack', 'hosting',
+    ]
+    slug_field = 'url_slug'
+
+    def form_valid(self, form):
+        form.instance.url_slug = form.instance.name.lower().replace(' ', '-')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('view-project', kwargs={'slug': self.object.url_slug})
 
 
 class ProjectDeleteView(DeleteView):
     model = Project
-    template_name = '/work/'
+    template_name = 'work/delete.html'
+    slug_field = 'url_slug'
+    success_url = '/work/'
 
 
 class ProjectCreateView(CreateView):
     model = Project
     template_name = 'work/create.html'
-    form_class = CreateUpdateProject
-    success_url = '/work/'
+    fields = [
+        'name', 'description', 'link', 'repository', 'languages', 'stack', 'hosting',
+    ]
+
+    def form_valid(self, form):
+        form.instance.url_slug = form.instance.name.lower().replace(' ', '-')
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('view-project', kwargs={'slug': self.object.url_slug})
