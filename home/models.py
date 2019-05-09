ab8dc0989e43e4f48ac2a18cdba6be9f84353013
Atsushi <todaatsushi0@gmail.com>
2019-05-09 17:36:29 +0100

    registered urls, moved models to relevatn app

--- home/models.py
+++ home/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 
-class Project(models.Model):
-    # Descriptive Info
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True)
-    link = models.CharField(null=True, max_length=100)
-    repository = models.CharField(max_length=100)
-
-    # Technical Info
-    languages = models.TextField()
-    hosting = models.CharField(max_length=100, null=True)
