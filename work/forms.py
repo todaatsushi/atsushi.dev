0f1241bf7d1d2d0286e0684903db94c045a6c62d
Atsushi <todaatsushi0@gmail.com>
2019-05-09 18:31:25 +0100

    added model form for project

---
+++ work/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from work.models import Project
+
+
+class CreateUpdateProject(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            'name', 'description', 'link', 'repository', 'languages', 'hosting',
+        ]
+        
