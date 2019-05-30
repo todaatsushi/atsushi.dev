3459afec6fea5eb55e49480f09a5263c7960b6be
Atsushi <todaatsushi0@gmail.com>
2019-05-30 20:31:44 +0100

    Updating model tests

--- work/tests/test_forms.py
+++ work/tests/test_forms.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
 
 from work.forms import CreateUpdateProject, CreateUpdateSpecs
 
@@ -25,8 +27,25 @@ class CreateProject(TestCase):
         form = CreateUpdateProject(data=data)
         self.assertFalse(form.is_valid())
 
+    def test_updating_project_works(self):
+        project = self.create_project()
+        data = project.__dict__
+
+        form = CreateUpdateProject(instance=project, data=data)
+        self.assertTrue(form.is_valid())
+
+        self.assertTrue(project.name == 'Test')
+
+        form.data['name'] = 'New name'
+
+        form.save()
+        self.assertTrue(project.name == 'New name')
+
 
 class UpdateProjectSpecs(TestCase):
+
+    def setUp(self):
+        super().setUp()
     
     def create_project(self):
         from work.tests.helper import create_project as cp
@@ -47,3 +66,19 @@ class UpdateProjectSpecs(TestCase):
 
         form = CreateUpdateSpecs(data=data)
         self.assertFalse(form.is_valid())
+
+    def test_updating_specs_works(self):
+        specs = self.create_project().projectspecs
+        data = specs.__dict__
+
+        form = CreateUpdateSpecs(instance=specs, data=data)
+        self.assertTrue(form.is_valid())
+
+        self.assertTrue(specs.technical_summary == 'To be added.')
+
+        form.data['technical_summary'] = 'Test summary'
+
+        form.save()
+        self.assertTrue(specs.technical_summary == 'Test summary')
+        
+        
