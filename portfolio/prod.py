33f33f9f4d2ed62b49944ee8c911ee59f059f51a
Atsushi <todaatsushi0@gmail.com>
2019-06-13 16:19:45 +0100

    Edited for mysql, added production db

--- portfolio/prod.py
+++ portfolio/prod.py
@@ -1,5 +1,20 @@
 from .base import *
 
+# Prod DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'TEST': {
+            'NAME': os.environ.get('DB_TEST_DB')
+        }
+    }
+}
+
+
 DEBUG = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
